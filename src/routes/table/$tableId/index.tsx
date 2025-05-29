e71653626c40d804037bbaf3d8f75f03840da433
Lukasz <lukasz.maniak22@gmail.com>
2025-05-29 20:19:26 +0200

    tablePageStructureAndStyling

--- src/routes/table/$tableId/index.tsx
+++ src/routes/table/$tableId/index.tsx
@@ -15,10 +15,45 @@ function RouteComponent() {
   const table: Table | undefined = tables.find((table) => table.id === tableId);
 
   return (
-    <div>
-      <Link to="/table">Go back</Link>
+    <div className="flex flex-col gap-y-6 items-start w-full">
+      <div className="flex items-center w-full justify-between ">
+        <Link
+          className="px-4 py-2 bg-green-200 rounded-4xl hover:bg-green-100 transition-all ease-in-out duration-200"
+          to="/table"
+        >
+          Go back
+        </Link>
+
+        <span className="flex items-center gap-x-2">
+          <Link
+            className="px-4 py-2 bg-green-200 rounded-4xl hover:bg-green-300 transition-all ease-in-out duration-200"
+            to="/table"
+          >
+            Edit Task
+          </Link>
+          <Link
+            className="px-4 py-2 bg-green-300 rounded-4xl hover:bg-green-200 transition-all ease-in-out duration-200"
+            to="/table"
+          >
+            Add New Task
+          </Link>
+        </span>
+      </div>
       <h1 className="text-3xl font-bold tracking-wider">{table?.title}</h1>
       <p>Created: {table?.timeCreate}</p>
+
+      {/* tasks list */}
+      <div className="flex items-center w-full justify-between">
+        <ul>
+          <p className="font-bold tracking-widest">TODO</p>
+        </ul>
+        <ul>
+          <p className="font-bold tracking-widest">DOING</p>
+        </ul>
+        <ul>
+          <p className="font-bold tracking-widest">DONE</p>
+        </ul>
+      </div>
     </div>
   );
 }
